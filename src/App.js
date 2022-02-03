dce4c978dc5cbac956e7dd076dcc98d0129bebbc
mariomolinaaraoz <mariomolinaaraoz@gmail.com>
2022-02-03 12:10:27 -0300

    correcion / homepage

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ export default function App() {
         <BrowserRouter>        
             <Navbar/>
             <Routes>
-                <Route path='http://mariomolinaaraoz.github.io/mario_molina/' element={<Home />}/>
-                <Route path="http://mariomolinaaraoz.github.io/mario_molina/mining" element={<Mineria />}/>
-                <Route path="http://mariomolinaaraoz.github.io/mario_molina/family" element={<Familia />}/>
+                <Route path='/mario_molina/' element={<Home />}/>
+                <Route path="/mario_molina/mining" element={<Mineria />}/>
+                <Route path="/mario_molina/family" element={<Familia />}/>
                 <Route path="*" element={<ErrorPage />}/>
             </Routes>
             <Footer/>
