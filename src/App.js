5a1989f0da2e68d2955548c1ec441ca5271c1c17
mariomolinaaraoz <mariomolinaaraoz@gmail.com>
2022-02-03 11:59:44 -0300

    correcion / homepage

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 
 export default function App() {
     return (        
-        <BrowserRouter>
+        <BrowserRouter>        
             <Navbar/>
             <Routes>
                 <Route path='/' element={<Home />}/>
